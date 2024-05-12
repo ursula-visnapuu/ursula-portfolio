f039dc4a13e6c10ff53592af82d321de2588fb44
Ursula Muts <ursula.muts@voco.ee>
2024-05-13 00:37:59 +0300

    back to top nupu parandus

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,6 +103,9 @@
         onscroll(document, headerFixed);
     }
 
+    /**
+     * Kas seda üldse vaja enam?
+     * 
     window.addEventListener("load", () => {
         // Kuula lehe skrollimise sündmusi
         window.addEventListener("scroll", toggleBacktotop);
@@ -118,22 +121,25 @@
                 });
             });
     });
+    
+    */
 
     /**
-   * Back to top button
-   
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
+     *  Back to top button
+     */
+
+    let backtotop = select(".back-to-top");
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add("active");
+            } else {
+                backtotop.classList.remove("active");
+            }
+        };
+        window.addEventListener("load", toggleBacktotop);
+        onscroll(document, toggleBacktotop);
     }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }*/
 
     /**
      * Mobile nav toggle
